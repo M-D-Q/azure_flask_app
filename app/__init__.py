a0f3a9ca4896d83b6ba1a4cf808f2035bc76742f
MDQ <maxdequick@gmail.com>
2023-01-20 11:42:31 +0100

    issou

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from flask_restful import Api
+
+from .profiles import profiles_blueprint
+from .api.profiles import ProfileResource
 
 app = Flask(__name__)
+api = Api(app, prefix="/api")
+
+app.register_blueprint(profiles_blueprint)
+
+api.add_resource(ProfileResource, "/profiles")
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
